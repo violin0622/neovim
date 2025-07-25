6c2d95a9808e98f9d698bd1b8bd6a33436ec8455
violin0622 <violin0622@outlook.com>
2025-07-25 11:52:08 +0800

    feat(lua/plugins/lualine.lua): one-char mode

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,14 +2,16 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
-      -- sections = {
-      --   lualine_a = {
-      --     "mode",
-      --     fmt = function(str)
-      --       return str:sub(1, 2)
-      --     end,
-      --   },
-      -- },
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            fmt = function(str)
+              return string.sub(str, 1, 1)
+            end,
+          },
+        },
+      },
     },
   },
 }
